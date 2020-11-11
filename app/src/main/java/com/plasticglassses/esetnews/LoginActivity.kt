ab4f67bfe168eaf6030a9766b459fb33f4a898f6
plasticglasses <33756800+plasticglasses@users.noreply.github.com>
2020-11-11 12:38:16 +0000

    added create user method to sign in form

--- app/src/main/java/com/plasticglassses/esetnews/LoginActivity.kt
+++ app/src/main/java/com/plasticglassses/esetnews/LoginActivity.kt
@@ -6,18 +6,38 @@ import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 public class LoginActivity : AppCompatActivity() {
 
-    private var mAuth: FirebaseAuth? = null
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        mAuth = FirebaseAuth.getInstance();
+        auth = Firebase.auth
 
+    }
+
+    @Override
+    public override fun onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            updateUI(currentUser)
+        }
+
+    }
+
+    private fun updateUI(currentUser: Any) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 
     fun clickLoginButton(view: View) {
@@ -29,7 +49,6 @@ public class LoginActivity : AppCompatActivity() {
         val intent = Intent(this, SignUpActivity::class.java)
         startActivity(intent)
     }
-
 
 
 //    override fun onStart() {
--- app/src/main/java/com/plasticglassses/esetnews/SignUpActivity.kt
+++ app/src/main/java/com/plasticglassses/esetnews/SignUpActivity.kt
@@ -8,7 +8,8 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_sign_up.*
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -19,47 +20,70 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        auth = Firebase.auth
+
+    }
+
+    @Override
+    public override fun onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            updateUI(currentUser)
+        }
+
+    }
+
+    private fun updateUI(currentUser: Any?) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun createUser(){
+        if (validPass()){
+
+            val email = findViewById<EditText>(R.id.signupEmail)
+            val password = findViewById<EditText>(R.id.signUpPassword)
+
+            auth.createUserWithEmailAndPassword(email.toString(), password.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        val snackbar = Snackbar.make(findViewById(android.R.id.content), "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+
+                        updateUI(null)
+                    }
+                }
+        }else{
+            //show issue with passwords
+        }
+
+
+    }
+
+    /*
+    Returns true if passwords are identical and false otherwise
+     */
+    private fun validPass(): Boolean {
+        val password1 = findViewById<EditText>(R.id.signUpPassword)
+        val password2 = findViewById<EditText>(R.id.signUpPassword2)
+
+        return password1.text.toString() == password2.text.toString()
     }
 
     /*
     uses fierbase to authenticate new users
      */
     fun clickLoginButton(view: View) {
-
-//        val intent = Intent(this, MainActivity::class.java)
-//        startActivity(intent)
-
-        val email = findViewById<EditText>(R.id.signupEmail)
-        val password = findViewById<EditText>(R.id.signUpPassword)
-
-        Snackbar.make(email, email.text.toString(), Snackbar.LENGTH_SHORT).show()
-        Snackbar.make(password, password.text.toString(), Snackbar.LENGTH_SHORT).show()
-
-
-
-//        auth.createUserWithEmailAndPassword(email.toString(), password.toString())
-//            .addOnCompleteListener(this) { task ->
-//                if (task.isSuccessful) {
-////
-//                    val snackbar = Snackbar.make(findViewById(android.R.id.content), "Authentication success.", Snackbar.LENGTH_SHORT).show()
-//
-//
-////                    // Sign in success, update UI with the signed-in user's information
-////                    Log.d(TAG, "createUserWithEmail:success")
-////                    val user = auth.currentUser
-////
-////                    val intent = Intent(this, MainActivity::class.java)
-////                    startActivity(intent)
-//
-//                } else {
-//                    // If sign in fails, display a message to the user.
-////                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-//
-//                    val snackbar = Snackbar.make(findViewById(android.R.id.content), "Authentication failed.", Snackbar.LENGTH_SHORT).show()
-//
-//                }
-//
-//            }
+        createUser()
     }
 
 }
