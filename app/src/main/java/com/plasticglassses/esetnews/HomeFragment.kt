fbbb7b2f53058a8e5b548884e276f81372bc9c27
plasticglasses <33756800+plasticglasses@users.noreply.github.com>
2020-11-17 14:16:01 +0000

    got recycler view working on home view

--- app/src/main/java/com/plasticglassses/esetnews/HomeFragment.kt
+++ app/src/main/java/com/plasticglassses/esetnews/HomeFragment.kt
@@ -6,7 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.dfl.newsapi.NewsApiRepository
+import com.dfl.newsapi.enums.Category
+import com.dfl.newsapi.enums.Country
+import com.plasticglassses.esetnews.adapters.NewsAdapter
+import io.reactivex.schedulers.Schedulers
 
 //import com.dfl.newsapi.NewsApiRepository
 //import com.dfl.newsapi.enums.Category
@@ -20,28 +26,131 @@ class HomeFragment : Fragment() {
 
     val newsApiRepository = NewsApiRepository("8abf9b3bbc4c4e86b186100f1c3f4e6d")
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        var rootView = inflater.inflate(R.layout.fragment_home, container, false)
+
+        generateGeneralNews()
+        generateScienceNews()
+        generateTechNews()
+
+        //setContentView(R.layout.fragment_home)
+        var headlineArrayList = ArrayList<newsModel>()
+        //get news
+
+        headlineArrayList = populateList()
+        //get text box on science fragment and add in a new headline
+
+        Log.d("mylist", headlineArrayList.toString())
+
+        //home fragment recycler view
+        //val headlineArrayList = populateList(myHeadlinelist, myImglist, myAuthorlist, myTimestamplist)
+
+        val recyclerHeadlineView = rootView.findViewById<View>(R.id.headline_recycler_view) as RecyclerView
+        //val headlineArrayList = genNews(recyclerHeadlineView)
+        val headlineLayoutManager = LinearLayoutManager(activity)
+        recyclerHeadlineView.layoutManager = headlineLayoutManager
+        val headlineAdapter = NewsAdapter(headlineArrayList)
+        recyclerHeadlineView.adapter = headlineAdapter
+
+        return rootView
+    }
+
+    private fun generateGeneralNews(): ArrayList<newsModel> {
+        val headlineArrayList = ArrayList<newsModel>()
+        //get general news for homepage
+        newsApiRepository.getTopHeadlines(category = Category.GENERAL, country = Country.GB, q = "", pageSize = 20, page = 1)
+            .subscribeOn(Schedulers.io())
+            .toFlowable()
+            .flatMapIterable { articles -> articles.articles }
+            .subscribe({ article -> Log.d("getTopHead General article", article.title + " " + article.urlToImage + " " + article.author + " " + article.publishedAt)
+
+                //if here since last updated time add to top_headline json
+
+            },
+                { t -> Log.d("getTopHeadlines error", t.message!!) })
+
+        return headlineArrayList
+
+    }
+
+    private fun generateScienceNews(): ArrayList<newsModel> {
+        val headlineArrayList = ArrayList<newsModel>()
+        //get general news for homepage
+        newsApiRepository.getTopHeadlines(category = Category.SCIENCE, country = Country.GB, q = "", pageSize = 20, page = 1)
+            .subscribeOn(Schedulers.io())
+            .toFlowable()
+            .flatMapIterable { articles -> articles.articles }
+            .subscribe({ article -> Log.d("getTopHead Science article", article.title + " " + article.urlToImage + " " + article.author + " " + article.publishedAt)
+
+                //if here since last updated time add to top_headline json
+
+            },
+                { t -> Log.d("getTopHeadlines error", t.message!!) })
+
+        return headlineArrayList
+
+    }
+
+    private fun generateTechNews(): ArrayList<newsModel> {
+        val headlineArrayList = ArrayList<newsModel>()
+        //get general news for homepage
+        newsApiRepository.getTopHeadlines(category = Category.TECHNOLOGY, country = Country.GB, q = "", pageSize = 20, page = 1)
+            .subscribeOn(Schedulers.io())
+            .toFlowable()
+            .flatMapIterable { articles -> articles.articles }
+            .subscribe({ article -> Log.d("getTopHead Technology article", article.title + " " + article.urlToImage + " " + article.author + " " + article.publishedAt)
+
+                //if here since last updated time add to top_headline json
+
+            },
+                { t -> Log.d("getTopHeadlines error", t.message!!) })
+
+        return headlineArrayList
+
+    }
+
+
+    private fun populateList(): ArrayList<newsModel> {
+        val list = ArrayList<newsModel>()
+        val myHeadlineList = arrayOf("Liz", "name1", "example2")
+        val myHeadlineImgList = arrayOf("Liz", "name", "example")
+        val myPublishersList = arrayOf("ET", "ETt", "ETs")
+        val myTimestampList = arrayOf("12:30", "12:30:59", "12:31")
+
+        for (i in 0..2){
+            val thisModel = newsModel()
+            thisModel.setHeadline(myHeadlineList[i].toString())
+            thisModel.setHeadlineImg(myHeadlineImgList[i].toString())
+            thisModel.setTimestamp(myTimestampList[i].toString())
+            thisModel.setPublisher(myPublishersList[i].toString())
+            list.add(thisModel)
+        }
+        return list
     }
 
 
 
-
-//    fun getHeadlines(view: View){
-//        val newsApiRepository = NewsApiRepository("8abf9b3bbc4c4e86b186100f1c3f4e6d")
+//    private fun populateList(myHeadlineList1: MutableList<String>, myHeadlineImgList1: MutableList<String>, myPublishersList1: MutableList<String>, myTimestampList1: MutableList<String>): MutableList<newsModel> {
+//        val list = ArrayList<newsModel>()
+//        val myHeadlineImgList = myHeadlineImgList1
+//        val myHeadlineList = myHeadlineList1
+//        val myPublishersList = myPublishersList1
+//        val myTimestampList = myTimestampList1
+//        var count = 0
+//        for (headline in myHeadlineList){
+//            count = count +1
+//        }
 //
-//        newsApiRepository.getTopHeadlines(category = Category.GENERAL, country = Country.US, q = "trump", pageSize = 20, page = 1)
-//            .subscribeOn(Schedulers.io())
-//            .toFlowable()
-//            .flatMapIterable { articles -> articles.articles }
-//            .subscribe({ article -> Log.d("getTopHead CC article", article.title) },
-//                { t -> Log.d("getTopHeadlines error", t.message.toString()) })
+//        for (i in 0..count-1){
+//            val thisModel = newsModel()
+//            thisModel.setHeadline(myHeadlineList[i].toString())
+//            thisModel.setHeadlineImg(myHeadlineImgList[i].toString())
+//            thisModel.setTimestamp(myTimestampList[i].toString())
+//            thisModel.setPublisher(myPublishersList[i].toString())
+//            list.add(thisModel)
+//        }
+//        return list
 //    }
-
 
 }
--- app/src/main/java/com/plasticglassses/esetnews/MainActivity.kt
+++ app/src/main/java/com/plasticglassses/esetnews/MainActivity.kt
@@ -44,136 +44,20 @@ class MainActivity : AppCompatActivity() {
         val tabSections = resources.getStringArray(R.array.tabSections)
 
         viewPager.adapter = TabAdapter(this)
-        TabLayoutMediator(tabLayout, viewPager, TabLayoutMediator.TabConfigurationStrategy{tab, position ->
-            when(position){
-                0 -> tab.text = tabSections[0]
-                1 -> tab.text = tabSections[1]
-                2 -> tab.text = tabSections[2]
-                3 -> tab.text = tabSections[3]
+        TabLayoutMediator(
+            tabLayout,
+            viewPager,
+            TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+                when (position) {
+                    0 -> tab.text = tabSections[0]
+                    1 -> tab.text = tabSections[1]
+                    2 -> tab.text = tabSections[2]
+                    3 -> tab.text = tabSections[3]
 
-            }
-        }).attach()
-
-        generateGeneralNews()
-        generateScienceNews()
-        generateTechNews()
-
-        setContentView(R.layout.fragment_home)
-        var headlineArrayList = ArrayList<newsModel>()
-        //get news
-
-        headlineArrayList = populateList()
-        //get text box on science fragment and add in a new headline
-
-        Log.d("mylist", headlineArrayList.toString())
-
-        //home fragment recycler view
-        //val headlineArrayList = populateList(myHeadlinelist, myImglist, myAuthorlist, myTimestamplist)
-
-        val recyclerHeadlineView = findViewById<View>(R.id.headline_recycler_view) as RecyclerView
-        //val headlineArrayList = genNews(recyclerHeadlineView)
-        val headlineLayoutManager = LinearLayoutManager(this)
-        recyclerHeadlineView.layoutManager = headlineLayoutManager
-        val headlineAdapter = NewsAdapter(headlineArrayList)
-        recyclerHeadlineView.adapter = headlineAdapter
-
-
+                }
+            }).attach()
     }
 
-    private fun generateGeneralNews(): ArrayList<newsModel> {
-        val headlineArrayList = ArrayList<newsModel>()
-        //get general news for homepage
-        newsApiRepository.getTopHeadlines(category = Category.GENERAL, country = Country.GB, q = "", pageSize = 20, page = 1)
-            .subscribeOn(Schedulers.io())
-            .toFlowable()
-            .flatMapIterable { articles -> articles.articles }
-            .subscribe({ article -> Log.d("getTopHead CC article", article.title + " " + article.urlToImage + " " + article.author + " " + article.publishedAt)
-
-                //if here since last updated time add to top_headline json
-
-            },
-                { t -> Log.d("getTopHeadlines error", t.message!!) })
-
-        return headlineArrayList
-
-    }
-
-    private fun generateScienceNews(): ArrayList<newsModel> {
-        val headlineArrayList = ArrayList<newsModel>()
-        //get general news for homepage
-        newsApiRepository.getTopHeadlines(category = Category.SCIENCE, country = Country.GB, q = "", pageSize = 20, page = 1)
-            .subscribeOn(Schedulers.io())
-            .toFlowable()
-            .flatMapIterable { articles -> articles.articles }
-            .subscribe({ article -> Log.d("getTopHead Science article", article.title + " " + article.urlToImage + " " + article.author + " " + article.publishedAt)
-
-                //if here since last updated time add to top_headline json
-
-            },
-                { t -> Log.d("getTopHeadlines error", t.message!!) })
-
-        return headlineArrayList
-
-    }
-
-    private fun generateTechNews(): ArrayList<newsModel> {
-        val headlineArrayList = ArrayList<newsModel>()
-        //get general news for homepage
-        newsApiRepository.getTopHeadlines(category = Category.TECHNOLOGY, country = Country.GB, q = "", pageSize = 20, page = 1)
-            .subscribeOn(Schedulers.io())
-            .toFlowable()
-            .flatMapIterable { articles -> articles.articles }
-            .subscribe({ article -> Log.d("getTopHead Technology article", article.title + " " + article.urlToImage + " " + article.author + " " + article.publishedAt)
-
-                //if here since last updated time add to top_headline json
-
-            },
-                { t -> Log.d("getTopHeadlines error", t.message!!) })
-
-        return headlineArrayList
-
-    }
-
-
-    private fun populateList(): ArrayList<newsModel> {
-        val list = ArrayList<newsModel>()
-        val myHeadlineList = arrayOf("Liz", "name1", "example2")
-        val myHeadlineImgList = arrayOf("Liz", "name", "example")
-        val myPublishersList = arrayOf("ET", "ETt", "ETs")
-        val myTimestampList = arrayOf("12:30", "12:30:59", "12:31")
-
-        for (i in 0..2){
-            val thisModel = newsModel()
-            thisModel.setHeadline(myHeadlineList[i].toString())
-            thisModel.setHeadlineImg(myHeadlineImgList[i].toString())
-            thisModel.setTimestamp(myTimestampList[i].toString())
-            thisModel.setPublisher(myPublishersList[i].toString())
-            list.add(thisModel)
-        }
-    return list
-    }
-
-//    private fun populateList(myHeadlineList1: MutableList<String>, myHeadlineImgList1: MutableList<String>, myPublishersList1: MutableList<String>, myTimestampList1: MutableList<String>): MutableList<newsModel> {
-//        val list = ArrayList<newsModel>()
-//        val myHeadlineImgList = myHeadlineImgList1
-//        val myHeadlineList = myHeadlineList1
-//        val myPublishersList = myPublishersList1
-//        val myTimestampList = myTimestampList1
-//        var count = 0
-//        for (headline in myHeadlineList){
-//            count = count +1
-//        }
-//
-//        for (i in 0..count-1){
-//            val thisModel = newsModel()
-//            thisModel.setHeadline(myHeadlineList[i].toString())
-//            thisModel.setHeadlineImg(myHeadlineImgList[i].toString())
-//            thisModel.setTimestamp(myTimestampList[i].toString())
-//            thisModel.setPublisher(myPublishersList[i].toString())
-//            list.add(thisModel)
-//        }
-//        return list
-//    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         //appbar
