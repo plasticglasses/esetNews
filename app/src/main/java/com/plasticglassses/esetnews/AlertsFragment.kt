e24ad56bfb1923b544afd034210d2aed8f26ba28
plasticglasses <33756800+plasticglasses@users.noreply.github.com>
2020-11-17 18:52:31 +0000

    add alert to firebase with certain id

--- app/src/main/java/com/plasticglassses/esetnews/AlertsFragment.kt
+++ app/src/main/java/com/plasticglassses/esetnews/AlertsFragment.kt
@@ -13,6 +13,8 @@ import com.google.android.material.chip.ChipGroup
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -27,6 +29,8 @@ class AlertsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        auth = Firebase.auth
+        val db = Firebase.firestore
         var rootView = inflater.inflate(R.layout.fragment_alerts, container, false)
         val alertChipGroup = rootView?.findViewById<ChipGroup>(R.id.alertChipGroup)
 
@@ -36,61 +40,102 @@ class AlertsFragment : Fragment() {
             //add a chip to the alerts fragment
             if (newAlertText != null) {
                 addAlert(rootView, inflater, alertChipGroup, newAlertText.text.toString())
+                //addAlertToFirebase(newAlertText.text.toString())
                 newAlertText.text.clear()
             }
 
         }
-        auth = Firebase.auth
-        getUsersAlerts(auth, rootView, inflater, alertChipGroup)
+
+
+        val uID = getUserID(auth)
+        val docID = getFirestoreID(uID, db)
+
+        val alertsList = arrayListOf<String>("Unity")
+        addAlertToFirebase(alertsList, db)
+
+        Log.d(TAG, ("uner the hanging tree " + uID))
+        Log.d(TAG, ("uner the hanging tree " + docID))
+
+//        var usersAlerts = getUsersAlerts(docID)
+//        //var alerts = getUsersAlerts(auth, rootView, inflater, alertChipGroup, uID, docID)
+//        if (usersAlerts != null) {
+//            Log.d(TAG, ("success we got the rabbi " + usersAlerts))
+//            for (alert in usersAlerts) {
+//                addAlert(rootView, inflater, alertChipGroup, alert.toString())
+//            }
+//        }
 
         return rootView
     }
 
-    private fun getUsersAlerts(
-        auth: FirebaseAuth,
-        rootView: View,
-        inflater: LayoutInflater,
-        alertChipGroup: ChipGroup?
-    ) {
+    /*
+    Get the id of the document which has the same authUserID sa the surrent loged in user
+     */
+    private fun getFirestoreID(uID: String, db: FirebaseFirestore): String {
+        var fID = ""
 
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                //for each user in firestore
+                for (document in result) {
+                    //get userid of the firestore document
+                    val docID = document.getString("authUserID")
+                    //check that its correct user
+                    if (docID == uID) {
+                        fID = document.id
+                        Log.d(TAG, ("found user firestore data" + fID))
+                    }
+                }
+            }
+        return fID
+    }
+
+    /*
+    get the current logged in user id
+     */
+    private fun getUserID(auth: FirebaseAuth): String {
         var thisUid = ""
-        //get users alerts from firebase
-        //get user id of current user logged in
+
         val user = FirebaseAuth.getInstance().currentUser
         if (user != null) {
             thisUid = user.uid
-            Log.d(TAG, ("success we got the rabbi"+ thisUid))
+            Log.d(TAG, ("success we got the UID" + thisUid))
 
             // User is signed in
         }
+        return thisUid
+    }
 
-        //go to firebase and get users information
-                val db = Firebase.firestore
-                db.collection("users")
-                    .get()
-                    .addOnSuccessListener { result ->
-                        //for each user in firestore
-                        for (document in result) {
-                            //get userid of the firestore document
-                            val value = document.getString("authUserID")
-                            //check that its correct user
-                            if (value == thisUid) {
-                                //get the list of alerts that users wants from firebase
-                                val username = document.getString("username")
-                                var usersAlerts = document["alerts"] as List<String>?
-                                //populate alerts fragment with chips of alerts
-                                if (usersAlerts != null) {
-                                    for (alert in usersAlerts){
-                                        addAlert(rootView, inflater, alertChipGroup, alert.toString())
-                                    }
-                                }
-                                Log.d(TAG, ("${document.id} => ${document.data} " + usersAlerts))
-                            }
-                        }
+    /*
+    Add alert to firebase
+     */
+    private fun addAlertToFirebase(alertsList: ArrayList<String>, db: FirebaseFirestore) {
+        var docID = "O4a9R4g9v9nooza5CELd"
+        // Update one field, creating the document if it does not already exist.
+        val data = hashMapOf("alerts" to alertsList)
 
-                //dynamically add the alert to the "current" chip group
+        db.collection("users").document("O4a9R4g9v9nooza5CELd")
+            .set(data, SetOptions.merge())
+    }
 
+    /*
+    get the current users alert array from firetore
+     */
+    private fun getUsersAlerts(
+        docID: String?
+    ): List<String>? {
+        var usersAlerts = listOf<String>()
+        val db = Firebase.firestore
+        db.collection("users").document(docID!!)
+            .get()
+            .addOnSuccessListener { result ->
+                //val username = result.getString("username")
+                usersAlerts = (result["alerts"] as List<String>?)!!
+                //populate alerts fragment with chips of alerts
+                Log.d(TAG, ("${result.id} => ${result.data} " +usersAlerts))
             }
+        return usersAlerts
     }
 
 
